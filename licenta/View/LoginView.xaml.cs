7942dc8e5aac91129ab6e71d7ba483e16b9a9514
leonardFlorin31 <leonard.beciu@student.unitbv.ro>
2025-02-05 21:49:03 +0200

    update pagina login

--- licenta/View/LoginView.xaml.cs
+++ licenta/View/LoginView.xaml.cs
@@ -17,4 +17,14 @@ public partial class LoginView : Window
             DragMove();
         }
     }
+
+    private void BtnMinimize_OnClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
 }
