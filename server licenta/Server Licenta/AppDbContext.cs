4fa3c1f3a43914bba33d333a036fe2b70a99d73a
leonardFlorin31 <leonard.beciu@student.unitbv.ro>
2025-02-26 18:10:38 +0200

    reconfig

---
+++ server licenta/Server Licenta/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server_Licenta.Controllers;
+
+namespace Server_Licenta
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configurați modelul dacă este necesar
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
