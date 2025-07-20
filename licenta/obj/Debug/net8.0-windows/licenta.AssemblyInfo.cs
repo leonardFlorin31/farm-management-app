354218c9cf6eac63722248087c97c11f22524e31
Beciu Leonard-Florin <147812069+leonardFlorin31@users.noreply.github.com>
2025-07-20 21:43:03 +0300

    updates

--- licenta/obj/Debug/net8.0-windows/licenta.AssemblyInfo.cs
+++ licenta/obj/Debug/net8.0-windows/licenta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("licenta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed120910f9945024efd1f74a2f8874615fa5bc87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+129c2a0846018466e4a6d4bed426c7092bb327c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("licenta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("licenta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
